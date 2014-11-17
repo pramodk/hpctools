58964bf82d1b9d4a9819915732e7473fd0111645
Pramod Kumbhar <pramod.s.kumbhar@gmail.com>
2014-11-17 13:41:19 +0100

    Update tau_mic.sh

--- lugmic/tau_mic.sh
+++ lugmic/tau_mic.sh
@@ -20,3 +20,75 @@ export PATH=/users/kumbhar/workarena/softwares/install/mic/tau_mpi_pdt_opari/mic
 ./configure -cc=mpiicc -c++=mpiicpc     -prefix=/users/kumbhar/workarena/softwares/install/x86/tau_mpi_pdt_opari  -iowrapper -pdt=/users/kumbhar/workarena/softwares/install/x86/pdt -pdt_c++=mpiicpc  -mpi -mpiinc=/apps/dommic/intel/impi/4.1.3.048/intel64/include -mpilib=/apps/dommic/intel/impi/4.1.3.048/intel64/lib -unwind=download -bfd=download -papi=/users/kumbhar/papi-5.3.0/x86 -opari -mpilibrary=-lmpi_mt
 make install
 
+
+
+From KNC-Login
+
+###TAU @ MICLOGIN - Pramod Kumbhar, Blue Brain Project @ EPFL######
+
+#!/bin/bash
+set -e
+
+#MODULES NEEDED FOR Tau
+package_names=(pdt tau)
+
+#LOAD DEFAULT MODULE
+module load intel impi
+
+#DIRECTORY UNDER WHICH ALL SOFTWARES WILL BE DOWNLOADED AND INSTALLED
+export INSTALL_DIR=$HOME/workarena/systems/jknc/softwares/install/profilers/tau
+export SOURCE_DIR=$HOME/workarena/systems/jknc/softwares/sources/profilers
+
+
+#INSTALL ALL DEPENDENCIES AND SCALASCA
+for package in ${package_names[*]}
+do
+
+    #SOURCE AND INSTALL DIRECTORY
+    source_dir="$SOURCE_DIR/"
+    install_dir="$INSTALL_DIR/"
+
+    #SET ENV VARIABLE FOR THAT SOFTWARE MODULE
+    env_prefix="${package^^}_INSTALL_DIR"
+    export ${env_prefix}="$install_dir"
+
+    #SOURCE DIRECTORY
+    mkdir -p $source_dir
+    mkdir -p $install_dir
+    cd $source_dir
+
+    echo "Installing package ${package^^} under ${install_dir}"
+
+    #INSTALLATION STEPS OF INDIVIDUAL PACKAGE:
+        #DOWNLOAD AND EXTRACT
+        #CONFIGURE
+        #MAKE AND MAKE INSTALL
+
+    case "$package" in
+
+
+        pdt)
+            #wget http://www.cs.uoregon.edu/research/paracomp/pdtoolkit/Download/pdt_latest.tar.gz
+            tar -xvzf pdt_latest.tar.gz
+            cd pdtoolkit-3.20
+
+            ./configure -ICPC -prefix=$PDT_INSTALL_DIR ./configure 
+
+            make -j12
+            make install
+            ;;
+
+        tau)
+            #wget http://www.cs.uoregon.edu/research/paracomp/tau/tauprofile/dist/tau_latest.tar.gz
+            #tar -xvzf tau_latest.tar.gz
+            cd tau-2.24
+
+            ./configure -prefix=$TAU_INSTALL_DIR -arch=mic_linux -cc=mpiicc -c++=mpiicpc -iowrapper -pdt=$PDT_INSTALL_DIR -pdt_c++=mpiicpc -mpi
+
+            make -j12
+            make install
+            ;;
+
+    esac
+
+done
